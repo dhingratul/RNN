6111b38c6f715372fc043099c69680d536b07c3d
dhingratul <atul.dhingra@rutgers.edu>
2017-06-16 07:45:21 -0400

    Updated Helpers

--- helpers.py
+++ helpers.py
@@ -8,6 +8,7 @@ Created on Thu Jun 15 20:54:58 2017
 
 import numpy as np
 
+
 def batch(inputs, max_sequence_length=None):
     """
     Args:
@@ -16,24 +17,21 @@ def batch(inputs, max_sequence_length=None):
         max_sequence_length:
             integer specifying how large should `max_time` dimension be.
             If None, maximum sequence length would be used
-    
     Outputs:
         inputs_time_major:
-            input sentences transformed into time-major matrix 
+            input sentences transformed into time-major matrix
             (shape [max_time, batch_size]) padded with 0s
         sequence_lengths:
-            batch-sized list of integers specifying amount of active 
+            batch-sized list of integers specifying amount of active
             time steps in each input sequence
     """
-    
     sequence_lengths = [len(seq) for seq in inputs]
     batch_size = len(inputs)
-    
+
     if max_sequence_length is None:
         max_sequence_length = max(sequence_lengths)
-    
-    inputs_batch_major = np.zeros(shape=[batch_size, max_sequence_length], dtype=np.int32) # == PAD
-    
+    inputs_batch_major = np.zeros(shape=[batch_size, max_sequence_length],
+                                  dtype=np.int32)  # == PAD
     for i, seq in enumerate(inputs):
         for j, element in enumerate(seq):
             inputs_batch_major[i, j] = element
@@ -58,11 +56,11 @@ def random_sequences(length_from, length_to,
         if length_from == length_to:
             return length_from
         return np.random.randint(length_from, length_to + 1)
-    
+
     while True:
         yield [
             np.random.randint(low=vocab_lower,
                               high=vocab_upper,
                               size=random_length()).tolist()
             for _ in range(batch_size)
-        ]
+            ]
